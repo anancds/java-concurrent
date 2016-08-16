d8dfd3debc6779f981b41b4e424757af0dcb8d27
anancds <anancds@163.com>
2016-08-16 16:29:37 +0800

    add Semaphore

--- src/main/java/com/cds/learn/tools/BankWaterService.java
+++ src/main/java/com/cds/learn/tools/BankWaterService.java
@@ -1,12 +1,45 @@
 package com.cds.learn.tools;
 
-import java.util.concurrent.CyclicBarrier;
+import java.util.Map;
+import java.util.concurrent.*;
 
-public class BankWaterService implements Runnable{
+public class BankWaterService implements Runnable {
 
     private CyclicBarrier c = new CyclicBarrier(4, this);
 
-    @Override public void run() {
+    private Executor executor = Executors.newFixedThreadPool(4);
 
+    private ConcurrentHashMap<String, Integer> sheetBankWaterCount = new ConcurrentHashMap<>();
+
+    private void count() {
+        for (int i = 0; i < 4; i++) {
+            executor.execute(new Runnable() {
+                @Override public void run() {
+                    sheetBankWaterCount.put(Thread.currentThread().getName(), 1);
+                    try {
+                        c.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override public void run() {
+        int result = 0;
+        for (Map.Entry<String, Integer> sheet : sheetBankWaterCount.entrySet()) {
+            result += sheet.getValue();
+        }
+
+        sheetBankWaterCount.put("result", result);
+        System.out.println(result);
+
+    }
+
+    public static void main(String[] args) {
+
+        BankWaterService bankWaterService = new BankWaterService();
+        bankWaterService.count();
     }
 }
