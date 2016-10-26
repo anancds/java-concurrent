66aeb9c96abea2f9bf9b63c35c4c8a515cc4f50a
anancds <anancds@163.com>
2016-10-26 13:02:09 +0800

    add volatile

---
+++ pcpp/src/main/java/com/cds/learn/chapter1/TestLongCounter.java
@@ -0,0 +1,29 @@
+package com.cds.learn.chapter1;
+
+import java.io.IOException;
+
+public class TestLongCounter {
+    public static void main(String[] args) throws IOException {
+        final LongCounter lc = new LongCounter();
+        Thread t = new Thread(() -> {
+            while (true)		// Forever call increment
+                lc.increment();
+        });
+        t.start();
+        System.out.println("Press Enter to get the current value:");
+        while (true) {
+            System.in.read();         // Wait for enter key
+            System.out.println(lc.get());
+        }
+    }
+}
+
+class LongCounter {
+    private long count = 0;
+    public synchronized void increment() {
+        count = count + 1;
+    }
+    public synchronized long get() {
+        return count;
+    }
+}
---
+++ pcpp/src/main/java/com/cds/learn/chapter1/VolatileTest.java
@@ -0,0 +1,31 @@
+package com.cds.learn.chapter1;
+
+public class VolatileTest {
+
+    public volatile static int count = 0;
+
+    public static void inc() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 100; i++) {
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                inc();
+            }
+        }).start();
+
+        VolatileTest.inc();
+        System.out.println("the count is: " + count);
+    }
+
+}
