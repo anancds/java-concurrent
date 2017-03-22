7288c89e94929891edf2d35cf55beb4f5b76cd51
anancds <anancds@163.com>
2017-03-22 16:46:19 +0800

    Changed Period

--- pcpp/src/main/java/com/cds/learn/chapter2/defensiveCopy/Period.java
+++ pcpp/src/main/java/com/cds/learn/chapter2/defensiveCopy/Period.java
@@ -1,38 +1,49 @@
 package com.cds.learn.chapter2.defensiveCopy;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public final class Period {
-    private final Date start;
-    private final Date end;
+	private final Date start;
+	private final Date end;
 
-    private Period(final Date start, final Date end) {
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException(start + " after " + end);
-        }
-        this.start = start;
-        this.end = end;
-//        this.start = new Date(start.getTime());
-//        this.end = new Date(end.getTime());
-        if(this.start.compareTo(this.end) > 0){
-            throw new IllegalArgumentException(this.start + " after " + this.end);
-        }
-    }
+	private Period(final Date start, final Date end) {
+		if (start.compareTo(end) > 0) {
+			throw new IllegalArgumentException(start + " after " + end);
+		}
+		this.start = start;
+		this.end = end;
+		//        this.start = new Date(start.getTime());
+		//        this.end = new Date(end.getTime());
+		if (this.start.compareTo(this.end) > 0) {
+			throw new IllegalArgumentException(this.start + " after " + this.end);
+		}
+	}
 
-    public Date start() {
-        return start;
-    }
+	public static void main(String[] args) {
+		SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
 
-    private Date end() {
-        return end;
-    }
-    //remainder omitted
+		Calendar calendar = Calendar.getInstance();
 
-    public static void main(String[] args) {
-        Date start = new Date();
-        Date end = new Date();
-        Period period = new Period(start, end);
-        end.setYear(2);
-        System.out.println(period.end());
-    }
+		calendar.setTime(new Date());
+		System.out.println(calendar.getTime());
+		Date start = new Date();
+		Date end = new Date();
+		Period period = new Period(start, end);
+
+		calendar.set(Calendar.YEAR, 2);
+		end.setTime(calendar.getTimeInMillis());
+
+		System.out.println(period.end());
+	}
+
+	public Date start() {
+		return start;
+	}
+	//remainder omitted
+
+	private Date end() {
+		return end;
+	}
 }
