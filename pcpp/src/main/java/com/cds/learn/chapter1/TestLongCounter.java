285bf3d8f42d8167d970b22da2755b800fb27d7f
anancds <anancds@163.com>
2017-08-18 11:04:32 +0800

    Added serviceloader package

--- pcpp/src/main/java/com/cds/learn/chapter1/TestLongCounter.java
+++ pcpp/src/main/java/com/cds/learn/chapter1/TestLongCounter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class TestLongCounter {
     public static void main(String[] args) throws IOException {
         final LongCounter lc = new LongCounter();
+
         Thread t = new Thread(() -> {
             while (true)		// Forever call increment
                 lc.increment();
@@ -12,7 +13,7 @@ public class TestLongCounter {
         t.start();
         System.out.println("Press Enter to get the current value:");
         while (true) {
-            System.in.read();         // Wait for enter key
+//            System.in.read();         // Wait for enter key
             System.out.println(lc.get());
         }
     }
@@ -21,7 +22,12 @@ public class TestLongCounter {
 class LongCounter {
     private long count = 0;
     public synchronized void increment() {
-        count = count + 1;
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      count = count + 1;
     }
     public synchronized long get() {
         return count;
